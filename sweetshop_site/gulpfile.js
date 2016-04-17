5c30a3f914f30cd4af15099601bd14acfb687284
matt <m@tt-thom.as>
2016-04-17 22:42:24 +0100

    First draft of site finished, finally

--- sweetshop_site/gulpfile.js
+++ sweetshop_site/gulpfile.js
@@ -1,29 +1,38 @@
 var gulp = require('gulp'),
-    bower = require('gulp-bower');
+    bower = require('gulp-bower'),
+    less = require('gulp-less');
 
 var config = {
     bowerDir: './bower_components',
     scriptDir: './static_contrib/scripts',
     cssDir: './static_contrib/content',
-    fontDir: './static_contrib/fonts'
+    fontDir: './static_contrib/fonts',
+    lessDir: './less'
 };
 
-gulp.task('default', ['copy']);
+gulp.task('default', ['copy', 'less']);
 
 gulp.task('bower', function () {
     return bower().pipe(gulp.dest(config.bowerDir));
+});
+
+gulp.task('less', function() {
+    return gulp.src(config.lessDir + '/creative.less').pipe(less()).pipe(gulp.dest(config.cssDir));
 });
 
 gulp.task('copy', ['bower'], function () {
 
     gulp.src([
         config.bowerDir + '/jquery/dist/*',
-        config.bowerDir + '/bootstrap/dist/js/bootstrap*'
+        config.bowerDir + '/jquery.easing/js/*',
+        config.bowerDir + '/bootstrap/dist/js/bootstrap*',
+        config.bowerDir + '/wow/dist/*'
     ]).pipe(gulp.dest(config.scriptDir));
 
     gulp.src([
         config.bowerDir + '/bootstrap/dist/css/*',
-        config.bowerDir + '/fontawesome/css/*'
+        config.bowerDir + '/fontawesome/css/*',
+        config.bowerDir + '/animate.css/*.css'
     ]).pipe(gulp.dest(config.cssDir));
 
     gulp.src([
